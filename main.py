af16b7cb630bc5db2251c52b91fc67641c34e19c
Muazzam Waheed <waheedmuazzam@gmail.com>
2024-06-01 14:05:41 -0400

    video test

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ip = socket.gethostbyname('www.google.com')
-port = 90
-s.connect((ip, port))
+# import socket
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# ip = socket.gethostbyname('www.google.com')
+# port = 90
+# s.connect((ip, port))
+
+import cv2 as cv
+
+capture = cv.VideoCapture(0)
+
+while True:
+
+    ret, frame = capture.read()
+    cv.imshow('frame',frame)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
